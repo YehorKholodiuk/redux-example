8722892e2798c5ce56c87458e9e3a21276f237df
ASUS <Yehor Kholodiuk>
2021-10-08 18:30:01 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {createStore} from 'redux';
 
+const reducers = (state, action) => {
+    console.log(state, action);
+    if (action.type === 'PLUS') {
+        return {
+            ...state,
+            count: state.count + 1
+        }
+    }
+    return {
+        count: 2,
+        appVersion: 'v0.0.1'
+    }
+}
+const store = createStore(reducers)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </Provider>
+    ,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
